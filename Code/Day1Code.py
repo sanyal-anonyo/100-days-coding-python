7e5ed75961dfde9671a329504b220bd3a72b1dac
sanyal-anonyo <anonyosanyal.portfolio@gmail.com>
2025-10-29 03:36:22 +0530

    Commit

---
+++ Code/Day1Code.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Genrator!")
+print("What's the name of your city you grew up in?")
+city=input()
+print("What's your pet name?")
+pet=input()
+
+print("Your band name could be", city, pet)
