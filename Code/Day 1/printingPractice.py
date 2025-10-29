a0b5b4d2f403a6d690c9fe003f57a928b161410e
sanyal-anonyo <anonyosanyal.portfolio@gmail.com>
2025-10-30 01:11:16 +0530

    Practice codes

---
+++ Code/Day 1/printingPractice.py
@@ -0,0 +1,39 @@
+print("Hello World!")
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("Hello" + "Anonyo")
+
+print("Hello"+" "+"Anonyo")
+
+# Fix the code below 👇
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+print("Hello " + input("What is your name?") + "!")
+
+name = input("What is your name?")
+print(name)
+
+print(len(input("What is your name?")))
+
+username=input("What is your name?")
+length = len(username)
+
+print(length)
+
+glass1 = "milk"
+glass2 = "juice"
+
+glass1, glass2 = glass2, glass1
+
