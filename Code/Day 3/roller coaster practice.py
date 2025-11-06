64365c112714d940e161b8e45c674708435f90be
sanyal-anonyo <anonyosanyal.portfolio@gmail.com>
2025-11-07 00:03:12 +0530

    Day 3 practice

--- Code/Day 3/roller coaster practice.py
+++ Code/Day 3/roller coaster practice.py
@@ -1,8 +1,13 @@
 print("Welcome to the roller coaster!")
 height = int(input("Please enter your height in cms: "))
+age = int(input("Please enter your age: "))
 
 if height >120:
-    print("You can ride the rollercoaster.")
+    if age > 18:
+        print("You can ride the rollercoaster and the ticket will cost $12")
+    else:
+        print("You can ride the rollercoaster and the ticket will cost $7")
+
 else:
     print("Sorry you have to grow taller before you can ride.")
 
