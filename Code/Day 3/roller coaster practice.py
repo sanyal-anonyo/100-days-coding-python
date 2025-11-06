5fc74587bc712ba30ee69a5070ba3b3e79d91769
sanyal-anonyo <anonyosanyal.portfolio@gmail.com>
2025-11-06 23:56:50 +0530

    Day 3 practice

---
+++ Code/Day 3/roller coaster practice.py
@@ -0,0 +1,8 @@
+print("Welcome to the roller coaster!")
+height = int(input("Please enter your height in cms: "))
+
+if height >120:
+    print("You can ride the rollercoaster.")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
